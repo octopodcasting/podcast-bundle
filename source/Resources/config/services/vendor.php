945135cee7962c45fc6a7f288c5bb451da926c3f
Tim Goudriaan <tim@codedmonkey.com>
2021-04-17 12:00:20 +0200

    Add crawler listeners to vendor mode service definitions

--- source/Resources/config/services/vendor.php
+++ source/Resources/config/services/vendor.php
@@ -5,6 +5,11 @@
 
 use Doctrine\ORM\EntityRepository;
 use Octopod\PodcastBundle\Command\CrawlEpisodesCommand;
+use Octopod\PodcastBundle\Crawler\EventListener\DoctrineFlushEntitiesListener;
+use Octopod\PodcastBundle\Crawler\EventListener\DoctrineMatchEpisodeListener;
+use Octopod\PodcastBundle\Crawler\EventListener\DoctrineMatchPodcastListener;
+use Octopod\PodcastBundle\Crawler\EventListener\DoctrinePersistEntitiesListener;
+use Octopod\PodcastBundle\Crawler\EventListener\MapCrawlingFieldsListener;
 use Octopod\PodcastBundle\Crawler\PodcastCrawler;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\param;
@@ -26,6 +31,43 @@ return static function (ContainerConfigurator $container) {
             null,
         ])
         ->tag('console.command')
+    ;
+
+    $container->set(DoctrineFlushEntitiesListener::class)
+        ->args([
+            service('doctrine.orm.entity_manager'),
+        ])
+        ->tag('kernel.event_listener', ['method' => 'onPostProcess'])
+    ;
+
+    $container->set(DoctrineMatchEpisodeListener::class)
+        ->args([
+            service('podcast.repository.episode'),
+            param('podcast.classes.episode'),
+            service('podcast.repository.podcast'),
+        ])
+        ->tag('kernel.event_listener', ['method' => 'onProcessEpisode', 'priority' => 4096])
+    ;
+
+    $container->set(DoctrineMatchPodcastListener::class)
+        ->args([
+            service('podcast.repository.podcast'),
+            param('podcast.classes.podcast'),
+        ])
+        ->tag('kernel.event_listener', ['method' => 'onProcessPodcast', 'priority' => 4096])
+    ;
+
+    $container->set(MapCrawlingFieldsListener::class)
+        ->tag('kernel.event_listener', ['method' => 'onProcessEpisode', 'priority' => 0])
+        ->tag('kernel.event_listener', ['method' => 'onProcessPodcast', 'priority' => 0])
+    ;
+
+    $container->set(DoctrinePersistEntitiesListener::class)
+        ->args([
+            service('doctrine.orm.entity_manager'),
+        ])
+        ->tag('kernel.event_listener', ['method' => 'onProcessEpisode', 'priority' => -4096])
+        ->tag('kernel.event_listener', ['method' => 'onProcessPodcast', 'priority' => -4096])
     ;
 
     $container->set('podcast.repository.episode', EntityRepository::class)
